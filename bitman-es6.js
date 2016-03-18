194f391ea0ff51396025553dc0c8c364f8b3ba2d
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-17 20:09:18 -0700

    Update bitman-es6.js

--- bitman-es6.js
+++ bitman-es6.js
@@ -6,7 +6,7 @@ ip=0
 stack=[]
 ret=[]
 c=''
-bits=32
+bits=1
 Array.prototype.pick=function(n){return this[this.length-n-1]}
 Array.prototype.chunk=function(r){var t,n=[],e=0;for(t=this.length/r;t>e;)n[e]=this.splice(0,r),e++;return n}
 //lookahead stuff
@@ -33,6 +33,7 @@ put=s=>out.textContent+=s
 //functions
 commands={
   	'1':x=>{stack[stack.length-1].push(1);stack[stack.length-1][0]?bits++:stack[stack.length-1].shift()},
+  	'0':x=>{stack[stack.length-1].push(0);stack[stack.length-1][0]?bits++:stack[stack.length-1].shift()},
   	'+':x=>{stack.push(Array(bits).fill(0))},
   	'$':x=>{stack.push(stack.pick(BigInteger(stack.pop().join``).toString(2)))},
   	'%':x=>{stack.pop()},
@@ -41,8 +42,7 @@ commands={
   	'|':x=>{x=stack.pop();stack.push(stack.pop().map((a,y)=>x[y]|a))},
   	'^':x=>{x=stack.pop();stack.push(stack.pop().map((a,y)=>x[y]^a))},
   	'~':x=>{stack.push(stack.pop().map(x=>+!x))},
-  	'<':x=>{stack[stack.length-1].shift();stack[stack.length-1].push(0)},
-  	'>':x=>{stack[stack.length-1].pop();stack[stack.length-1].shift(0)},
+  	'>':x=>{stack[stack.length-1].pop()},
 	'[':x=>{stack.push([...("0".repeat(bits)+(ip>>>0).toString(2)).slice(-bits)].map(x=>+x));ip=matching_brace()},
 	']':x=>{ip=ret.pop()},
 	'!':x=>{ret.push(ip);ip=+BigInteger(stack.pop()).toString(2)},
@@ -53,7 +53,7 @@ commands={
 //good-to-know data for runtime
 log=_=>stats.innerHTML=`Code          │ ${format=[...code.replace(/[\x00-\x1f]/g,x=>String.fromCharCode(x.charCodeAt()+9216))],format[ip]=`<span style=background-color:#7ec0ee>${format[ip]||""}</span>`,format.join``}
 IP            │ ${ip}
-Stack         │ ${stack.length?stack.map(x=>x.join``).join` `:''}
+Stack         │ ${stack.length?stack.slice(0).reverse().map(x=>x.join``).join`\n`:''}
 Return Stack  │ ${JSON.stringify(ret)}`
 nsc.oninput=onload=_=>(code=nsc.value,log())
 
@@ -61,11 +61,11 @@ nsc.oninput=onload=_=>(code=nsc.value,log())
 parse=_=>{
 	c=code[ip]
 	log()
-	if(commands[c])commands[c]();ip++
+	if(commands[c])commands[c]();stack=stack.map(x=>Array(bits).fill(0).concat(x).slice(-bits));ip++
 }
 
 //clearing everything before starting prog
-init=_=>(code=nsc.value,ahead=[],ip=0,bits=32,stack=[],ret=[],console.clear())
+init=_=>(code=nsc.value,ahead=[],ip=0,bits=1,stack=[],ret=[],console.clear())
 
 //determines either full or timed run
 run=_=>{init();if(time.checked)interval=setInterval('ip<code.length?parse():(clearInterval(interval),log())',ms.value||1);else for(;ip<code.length;)parse();log()}
