7c537da468b1b0ade53fb2ac7c26235a21fd4bc0
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-03-17 20:09:29 -0700

    Update bitman.js

--- bitman.js
+++ bitman.js
@@ -1,5 +1,5 @@
 var _templateObject = _taggedTemplateLiteral([''], ['']),
-    _templateObject2 = _taggedTemplateLiteral([' '], [' ']);
+    _templateObject2 = _taggedTemplateLiteral(['\n'], ['\\n']);
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
@@ -13,7 +13,7 @@ ip = 0;
 stack = [];
 ret = [];
 c = '';
-bits = 32;
+bits = 1;
 Array.prototype.pick = function (n) {
 	return this[this.length - n - 1];
 };
@@ -58,6 +58,9 @@ commands = {
 	'1': function _(x) {
 		stack[stack.length - 1].push(1);stack[stack.length - 1][0] ? bits++ : stack[stack.length - 1].shift();
 	},
+	'0': function _(x) {
+		stack[stack.length - 1].push(0);stack[stack.length - 1][0] ? bits++ : stack[stack.length - 1].shift();
+	},
 	'+': function _(x) {
 		stack.push(Array(bits).fill(0));
 	},
@@ -90,11 +93,8 @@ commands = {
 			return +!x;
 		}));
 	},
-	'<': function _(x) {
-		stack[stack.length - 1].shift();stack[stack.length - 1].push(0);
-	},
 	'>': function _(x) {
-		stack[stack.length - 1].pop();stack[stack.length - 1].shift(0);
+		stack[stack.length - 1].pop();
 	},
 	'[': function _(x) {
 		stack.push([].concat(_toConsumableArray(("0".repeat(bits) + (ip >>> 0).toString(2)).slice(-bits))).map(function (x) {
@@ -121,7 +121,7 @@ commands = {
 log = function log(_) {
 	return stats.innerHTML = 'Code          │ ' + (format = [].concat(_toConsumableArray(code.replace(/[\x00-\x1f]/g, function (x) {
 		return String.fromCharCode(x.charCodeAt() + 9216);
-	}))), format[ip] = '<span style=background-color:#7ec0ee>' + (format[ip] || "") + '</span>', format.join(_templateObject)) + '\nIP            │ ' + ip + '\nStack         │ ' + (stack.length ? stack.map(function (x) {
+	}))), format[ip] = '<span style=background-color:#7ec0ee>' + (format[ip] || "") + '</span>', format.join(_templateObject)) + '\nIP            │ ' + ip + '\nStack         │ ' + (stack.length ? stack.slice(0).reverse().map(function (x) {
 		return x.join(_templateObject);
 	}).join(_templateObject2) : '') + '\nReturn Stack  │ ' + JSON.stringify(ret);
 };
@@ -133,12 +133,14 @@ nsc.oninput = onload = function onload(_) {
 parse = function parse(_) {
 	c = code[ip];
 	log();
-	if (commands[c]) commands[c]();ip++;
+	if (commands[c]) commands[c]();stack = stack.map(function (x) {
+		return Array(bits).fill(0).concat(x).slice(-bits);
+	});ip++;
 };
 
 //clearing everything before starting prog
 init = function init(_) {
-	return code = nsc.value, ahead = [], ip = 0, bits = 32, stack = [], ret = [], console.clear();
+	return code = nsc.value, ahead = [], ip = 0, bits = 1, stack = [], ret = [], console.clear();
 };
 
 //determines either full or timed run
